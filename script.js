a1f04f358f129683f41b7ecf05a1112347f31d68
VincentComeau24 <comeauwwe@gmail.com>
2026-07-17 08:59:28 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const button = document.getElementById("funFactBtn");
+const funFact = document.getElementById("funFact");
+
+button.addEventListener("click", function () {
+
+    if (funFact.style.display === "none") {
+        funFact.style.display = "block";
+        button.textContent = "Hide Fun Fact";
+    } else {
+        funFact.style.display = "none";
+        button.textContent = "Show Fun Fact";
+    }
+
+});
